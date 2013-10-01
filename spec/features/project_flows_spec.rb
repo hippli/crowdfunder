d97f200374186145dc72fea2a36845ffb1e9884a
hippli <philip.e.harrison@gmail.com>
2013-10-01 18:28:52 -0400

    Writing test for active navbar element

--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -55,6 +55,14 @@ describe "Project Listing" do
       page.should have_selector('.navbar ul li.active a', text: "Projects") 
       expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
 
+      # Create a project to visit its show page at the end of the test
+      project1 = FactoryGirl.create(:project, :title => "Project 1")
+
+      # On a project's show page, the Projects nav element should still be active
+      click_link 'Project 1'
+      page.should have_selector('.navbar ul li.active a', text: "Projects")
+      expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
+
 
     end    
     
